636dbb3c5ec49f2915020254f0c42946b5e2fb61
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-03-28 18:23:47 -0300

    criando os models para o client

---
+++ src/app/services/api-client/clients/client.models.ts
@@ -0,0 +1,39 @@
+export interface SaveClientRequest {
+    name: string
+    email: string
+    phone: string
+}
+
+export interface UpdateClientRequest {
+    name: string
+    email: string
+    phone: string
+}
+
+export interface SaveClientResponse {
+    id: number
+    name: string
+    email: string
+    phone: string
+}
+
+export interface ResponseClientResponse {
+    id: string
+    name: string
+    email: string
+    phone: string
+}
+
+export interface ListClientResponse {
+    id: string
+    name: string
+    email: string
+    phone: string
+}
+
+export interface DetailClientResponse {
+    id: string
+    name: string
+    email: string
+    phone: string
+}
