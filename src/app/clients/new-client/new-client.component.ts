5b5c142b087b80128f21db1958a9306493fa2bc7
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-03-31 22:07:52 -0300

    terminando o cadastro de cliente

--- src/app/clients/new-client/new-client.component.ts
+++ src/app/clients/new-client/new-client.component.ts
@@ -1,20 +1,42 @@
-import { Component, Inject } from '@angular/core';
-import {  SERVICES_TOKEN } from '../../services/service.token';
+import { Component, Inject, OnDestroy } from '@angular/core';
+import { SERVICES_TOKEN } from '../../services/service.token';
 import { IClientService } from '../../services/api-client/clients/iclient.service';
 import { ClientsService } from '../../services/api-client/clients/clients.service';
-import { ClientFormComponent } from "../components/client-form/client-form.component";
+import { ClientFormComponent } from '../components/client-form/client-form.component';
+import { ClientModelForm } from '../client.models';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { ISnackbarManagerService } from '../../services/isnaskbar-manager.service';
 
 @Component({
   selector: 'app-new-client',
   imports: [ClientFormComponent],
   templateUrl: './new-client.component.html',
   styleUrl: './new-client.component.scss',
-  providers:[
-    {provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService}
-  ]
+  providers: [
+    { provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService },
+  ],
 })
-export class NewClientComponent {
+export class NewClientComponent implements OnDestroy {
+  private httpSubscription?: Subscription;
 
-  constructor(@Inject(SERVICES_TOKEN.HTTP.CLIENT) private readonly httpService: IClientService){}
+  constructor(
+    @Inject(SERVICES_TOKEN.HTTP.CLIENT) private readonly httpService: IClientService,
+    @Inject(SERVICES_TOKEN.SNACKBAR) private readonly snackBarManager: ISnackbarManagerService,
+    private readonly router: Router
+  ) {}
 
+  ngOnDestroy(): void {
+    if (this.httpSubscription) {
+      this.httpSubscription.unsubscribe();
+    }
+  }
+
+  onSubmitClient(value: ClientModelForm) {
+    const { id, ...request } = value;
+    this.httpService.save(request).subscribe((_) => {
+      this.snackBarManager.show('Usuário cadastrado')
+      this.router.navigate(['clients/list']);
+    });
+  }
 }
--- src/app/services/service.token.ts
+++ src/app/services/service.token.ts
@@ -1,9 +1,11 @@
 import { InjectionToken } from "@angular/core";
 import { IClientService } from "./api-client/clients/iclient.service";
+import { ISnackbarManagerService } from "./isnaskbar-manager.service";
 
 export const SERVICES_TOKEN = {
     HTTP :{
         CLIENT: new InjectionToken<IClientService>('SERVICES_TOKEN_HTTP.CLIENT'),
         //SCHEDULE: new InjectionToken<IScheduleService>('SERVICES_TOKEN_HTTP.SCHEDULE')
-    }
+    },
+    SNACKBAR: new InjectionToken<ISnackbarManagerService>('SERVICES_TOKEN.SNACKBAR')
 }
