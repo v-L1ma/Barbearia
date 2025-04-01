e5192a28fc85c728baaca347676e696fc3381dde
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-04-01 18:16:54 -0300

    criando header e menu de navegação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { CardHeaderComponent } from "./commons/components/card-header/card-header.component";
+import { filter, map, Subscription } from 'rxjs';
+import { MenuBarComponent } from './commons/components/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardHeaderComponent, MenuBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'barbearia';
+
+  private routeSubscription?: Subscription;
+
+  constructor(private readonly router: Router, private readonly activateRoute: ActivatedRoute){}
+
+  ngOnDestroy(): void {
+    if(this.routeSubscription) {
+      this.routeSubscription.unsubscribe()
+    }
+  }
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(()=>this.getRouteTitle(this.activateRoute))
+    ).subscribe()
+  }
+
+  private getRouteTitle(route: ActivatedRoute):string {
+    let child = route;
+    while(child.firstChild){
+      child = child.firstChild;
+    }
+    return child.snapshot.data['title'] || 'Default Ttle';
+  }
 }
